377b899026280eb81773f3f77ec01b5676d563c9
demoofbug <demoofbug@qq.com>
2025-08-26 07:05:37 +0800

    refactor(nativeInterop): optimize libsecret/wincred config and build

--- klib-libsecret/build.gradle.kts
+++ klib-libsecret/build.gradle.kts
@@ -1,22 +1,18 @@
 import com.getiox.gradle.buildsrc.PkgConfig
-import com.getiox.gradle.buildsrc.isLinux
 import com.getiox.gradle.buildsrc.skipBuildIfNotLinux
 
 skipBuildIfNotLinux()
 
 val pkg = PkgConfig(project)
 kotlin {
-    if (isLinux) {
-        listOf(linuxX64(), linuxArm64()).forEach {
-            it.compilations.getByName("main") {
-                val libsecret by cinterops.creating {
-                    defFile("src/nativeInterop/cinterop/libsecret.def")
-                    packageName("org.gnome.libsecret")
+    listOf(linuxX64(), linuxArm64()).forEach {
+        it.compilations.getByName("main") {
+            val libsecret by cinterops.creating {
+                defFile("src/nativeInterop/cinterop/libsecret.def")
 
-                    // Get compile and link flags via pkg-config
-                    compilerOpts.addAll(pkg.compilerFlags("libsecret-1"))
-                    linkerOpts.addAll(pkg.linkerFlags("libsecret-1"))
-                }
+                // Get compile and link flags via pkg-config
+                compilerOpts += pkg.compilerFlags("libsecret-1")
+                linkerOpts += pkg.linkerFlags("libsecret-1")
             }
         }
     }
--- klib-wincred/build.gradle.kts
+++ klib-wincred/build.gradle.kts
@@ -1,19 +1,12 @@
-import com.getiox.gradle.buildsrc.isWindows
 import com.getiox.gradle.buildsrc.skipBuildIfNotWindows
 
 skipBuildIfNotWindows()
 
 kotlin {
-    if(isWindows) {
-        mingwX64 {
-            compilations.getByName("main") {
-                val wincred by cinterops.creating {
-                    defFile("src/nativeInterop/cinterop/wincred.def")
-                    packageName("com.microsoft.wincred")
-
-                    // Link Windows system libraries
-                  //  linkerOpts.addAll(listOf("-ladvapi32", "-lcredui"))
-                }
+    mingwX64 {
+        compilations.getByName("main") {
+            val wincred by cinterops.creating {
+                defFile("src/nativeInterop/cinterop/wincred.def")
             }
         }
     }
